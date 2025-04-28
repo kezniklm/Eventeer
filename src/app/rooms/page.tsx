0a75893df4ab38780d658662726fb86d482277b7
Matej Keznikl <matej.keznikl@outlook.com>
2025-04-28 17:14:08 +0200

    ADD: metadata to rooms page

--- src/app/rooms/page.tsx
+++ src/app/rooms/page.tsx
@@ -1,3 +1,11 @@
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Rooms",
+  description:
+    "View and manage your rooms on Eventeer. Track events, expenses, and collaborate with others in shared spaces."
+};
+
 const RoomsPage = () => (
   <div>
     <button className="btn btn-primary">Button</button>
