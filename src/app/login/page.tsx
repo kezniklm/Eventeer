f290830be7f80b988f05a10bfddc05f5a203b193
Matej Keznikl <matej.keznikl@outlook.com>
2025-04-28 17:10:42 +0200

    ADD: metadata to login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,10 @@
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Login",
+  description: "Sign in to Eventeer and start organizing your events, tasks, and expenses with ease."
+};
+
 const LoginPage = () => (
   <div>
     <button className="btn btn-primary">Button</button>
