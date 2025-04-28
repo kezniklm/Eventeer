cb829993e5c542adc172272ae2afd4954880a583
Matej Keznikl <matej.keznikl@outlook.com>
2025-04-28 17:12:43 +0200

    ADD: metadata to profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,10 @@
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Profile",
+  description: "View and manage your profile on Eventeer. Edit your personal details, track your events, and more."
+};
+
 const ProfilePage = () => (
   <div>
     <button className="btn btn-primary">Button</button>
