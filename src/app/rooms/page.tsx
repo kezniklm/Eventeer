dc9ebf4c26630af466f304efa60eb394121458f2
Matej Keznikl <matej.keznikl@outlook.com>
2025-04-30 21:15:54 +0200

    FIX: lint errors

--- src/app/rooms/page.tsx
+++ src/app/rooms/page.tsx
@@ -9,15 +9,13 @@ export const metadata: Metadata = {
     "View and manage your rooms on Eventeer. Track events, expenses, and collaborate with others in shared spaces."
 };
 
-const RoomsPage = () => {
-  return (
-    <div className="space-y-6">
-      <h1 className="text-center text-6xl">Rooms</h1>
-      <Rooms />
-      <h1 className="text-center mb-6">Invitations</h1>
-      <RoomInvitations />
-    </div>
-  );
-};
+const RoomsPage = () => (
+  <div className="space-y-6">
+    <h1 className="text-center text-6xl">Rooms</h1>
+    <Rooms />
+    <h1 className="text-center mb-6">Invitations</h1>
+    <RoomInvitations />
+  </div>
+);
 
 export default RoomsPage;
