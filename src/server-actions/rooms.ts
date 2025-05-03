27c0bf817f313e0596554a1a124ec6f5aacef810
Matej Keznikl <matej.keznikl@outlook.com>
2025-05-03 23:12:20 +0200

    ADD: rooms server actions

---
+++ src/server-actions/rooms.ts
@@ -0,0 +1,20 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+
+import { deleteUserHasRoom, updateUserHasRoom } from "@/repository/rooms";
+
+export const leaveRoomAction = async (roomId: number, userId: string) => {
+  await deleteUserHasRoom(roomId, userId);
+  revalidatePath("/rooms");
+};
+
+export const acceptRoomInvitationAction = async (roomId: number, userId: string) => {
+  await updateUserHasRoom(roomId, userId, true);
+  revalidatePath("/rooms");
+};
+
+export const declineRoomInvitationAction = async (roomId: number, userId: string) => {
+  await deleteUserHasRoom(roomId, userId);
+  revalidatePath("/rooms");
+};
