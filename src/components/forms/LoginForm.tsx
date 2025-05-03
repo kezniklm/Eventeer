51fd1a842eafd1ae285ff92ecb9e9af5e2c43385
Martin Prochazka <pro.nitram@gmail.com>
2025-05-03 13:17:48 +0200

    Removed imports

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -2,9 +2,6 @@ import { type HTMLAttributes } from "react";
 
 import { providerMap, signIn } from "@/auth";
 import { ProviderButton } from "@/components/providers/provider-button";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { cn } from "@/lib/utils";
 
 type LoginFormProps = {
