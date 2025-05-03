bd8f5efbe1034fdc3358617ef8bb506551d86046
Martin Prochazka <pro.nitram@gmail.com>
2025-05-03 10:43:48 +0200

    Unstyled login

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -1,21 +1,39 @@
+import { providerMap, signIn } from "@/auth";
+import { GoogleIcon } from "@/components/providers/GoogleIcon";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { PageHeading } from "@/components/ui/page-heading";
 
 export const LoginForm = () => (
-  <div className="space-y-4">
-    <div className="grid w-full max-w-sm items-center gap-1.5">
-      <Label htmlFor="email">Email</Label>
-      <Input type="email" id="email" placeholder="Email" />
+  <div className="m-auto container flex flex-grow items-center flex-col text-center">
+    <PageHeading>Sign In</PageHeading>
+    <div className="space-y-4 container m-auto w-100 p-5">
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+        <Label htmlFor="email">Email</Label>
+        <Input type="email" id="email" placeholder="Email" />
+      </div>
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+        <Label htmlFor="nickname">Nickname</Label>
+        <Input type="nickname" id="nickname" placeholder="Nickname" />
+      </div>
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+        <Label htmlFor="password">Password</Label>
+        <Input type="password" id="password" placeholder="Password" />
+      </div>
+      <Button>Button</Button>
+      {Object.values(providerMap).map((provider) => (
+        <form
+          key={provider.id}
+          action={async () => {
+            "use server";
+            await signIn(provider.id);
+          }}
+        >
+          <button type="submit">Sign in with {provider.name}</button>
+        </form>
+      ))}
+      <GoogleIcon />
     </div>
-    <div className="grid w-full max-w-sm items-center gap-1.5">
-      <Label htmlFor="nickname">Nickname</Label>
-      <Input type="nickname" id="nickname" placeholder="Nickname" />
-    </div>
-    <div className="grid w-full max-w-sm items-center gap-1.5">
-      <Label htmlFor="password">Password</Label>
-      <Input type="password" id="password" placeholder="Password" />
-    </div>
-    <Button>Button</Button>
   </div>
 );
