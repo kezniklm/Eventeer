276be08ac50431f6cd69fb164b2fbf6e6f4154f8
Matej Keznikl <matej.keznikl@outlook.com>
2025-05-03 23:12:13 +0200

    ADD: rooms repositories

---
+++ src/repository/rooms.ts
@@ -0,0 +1,48 @@
+import { and, eq } from "drizzle-orm";
+
+import { db } from "@/db";
+import { users } from "@/db/schema/auth";
+import { room, userHasRoom } from "@/db/schema/room";
+
+export const getMemberRoomsForUser = async (userId: string) => {
+  const result = await db
+    .select({ room })
+    .from(room)
+    .innerJoin(userHasRoom, eq(userHasRoom.room_id, room.id))
+    .where(and(eq(userHasRoom.user_id, userId), eq(userHasRoom.invitation_state, true)));
+
+  return result.map((r) => r.room);
+};
+
+export const getInvitedRoomsForUser = async (userId: string) => {
+  const result = await db
+    .select({ room })
+    .from(room)
+    .innerJoin(userHasRoom, eq(userHasRoom.room_id, room.id))
+    .where(and(eq(userHasRoom.user_id, userId), eq(userHasRoom.invitation_state, false)));
+
+  return result.map((r) => r.room);
+};
+
+export const getRoomUsersNames = async (roomId: number) => {
+  const result = await db
+    .select({
+      name: users.name
+    })
+    .from(users)
+    .innerJoin(userHasRoom, eq(userHasRoom.user_id, users.id))
+    .where(eq(userHasRoom.room_id, roomId));
+
+  return result.map((r) => r.name).filter((name): name is string => name !== null);
+};
+
+export const deleteUserHasRoom = async (roomId: number, userId: string) => {
+  await db.delete(userHasRoom).where(and(eq(userHasRoom.room_id, roomId), eq(userHasRoom.user_id, userId)));
+};
+
+export const updateUserHasRoom = async (roomId: number, userId: string, invitation_state: boolean) => {
+  await db
+    .update(userHasRoom)
+    .set({ invitation_state })
+    .where(and(eq(userHasRoom.room_id, roomId), eq(userHasRoom.user_id, userId)));
+};
