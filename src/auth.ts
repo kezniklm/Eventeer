ef56fff7b05a5d734dc95ac659625382e270ff84
Martin Prochazka <pro.nitram@gmail.com>
2025-05-03 10:43:48 +0200

    Unstyled login

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,18 @@
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
+import { type Provider } from "next-auth/providers";
+import Google from "next-auth/providers/google";
 
-export const { handlers, auth } = NextAuth({ providers: [GitHub] });
+const providers: Provider[] = [Google];
+
+export const providerMap = providers
+  .map((provider) => {
+    if (typeof provider === "function") {
+      const providerData = provider();
+      return { id: providerData.id, name: providerData.name };
+    } else {
+      return { id: provider.id, name: provider.name };
+    }
+  })
+  .filter((provider) => provider.id !== "credentials");
+
+export const { signIn, signOut, handlers, auth } = NextAuth({ providers });
