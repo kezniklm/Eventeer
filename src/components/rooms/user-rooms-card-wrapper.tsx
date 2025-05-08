45d5e9bec89561401e7490ec88a5b8b9926ff2ed
Matej Keznikl <matej.keznikl@outlook.com>
2025-05-08 11:09:09 +0200

    ADD: correct link propagation to user rooms card

--- src/components/rooms/user-rooms-card-wrapper.tsx
+++ src/components/rooms/user-rooms-card-wrapper.tsx
@@ -6,7 +6,7 @@ import { type Result } from "@/result";
 import { RoomCard } from "./room-card";
 
 type Props = {
-  room: { id: number; name: string };
+  room: { id: number; name: string; link: string };
   userId: string;
   badges: string[];
 };
@@ -35,7 +35,7 @@ export const RoomCardWrapper = ({ room, userId, badges }: Props) => {
     <RoomCard
       id={room.id}
       title={room.name}
-      linkName={room.name}
+      linkName={room.link}
       badges={badges}
       handleLeave={handleLeave}
       handleAddUser={handleAddUser}
