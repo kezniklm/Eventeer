30076a199c04552d99411eff0874d342f2ab8578
Martin Prochazka <pro.nitram@gmail.com>
2025-05-11 19:09:07 +0200

    progress

--- src/db/zod/event.ts
+++ src/db/zod/event.ts
@@ -1,15 +1,17 @@
 import { createInsertSchema } from "drizzle-zod";
-import { type z } from "zod";
+import { z } from "zod";
 
 import { settleUp } from "../schema/activity";
 
 export const createSettleUpSchema = createInsertSchema(settleUp);
 export type CreateSettleUp = z.infer<typeof createSettleUpSchema>;
 
-export const settleUpFormSchema = createSettleUpSchema.pick({
-  description: true,
-  isPublic: true,
-  priority: true,
-  name: true
-});
+export const settleUpFormSchema = createSettleUpSchema
+  .pick({
+    description: true,
+    isPublic: true,
+    priority: true,
+    name: true
+  })
+  .extend({ users: z.array(z.object({ name: z.string(), id: z.number() })) }); // TODO:
 export type SettleUpForm = z.infer<typeof settleUpFormSchema>;
