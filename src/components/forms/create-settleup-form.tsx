8a7f52a7f039c3031a1db3a589dd48202b163ce1
Martin Prochazka <pro.nitram@gmail.com>
2025-05-11 18:41:26 +0200

    change inputs

--- src/components/forms/create-settleup-form.tsx
+++ src/components/forms/create-settleup-form.tsx
@@ -1,95 +1,93 @@
 "use client";
 
-import * as React from "react";
+import { FormProvider, useForm } from "react-hook-form";
 
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
-import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { type SettleUpForm } from "@/db/zod/event";
 import { useRoomContext } from "@/context/room-context";
 
+import { FormInput } from "../ui/form-input";
+
 export const CreateSettleUpForm = () => {
+  const form = useForm<SettleUpForm>();
   const _roomInfo = useRoomContext();
 
   return (
-    <div className="flex flex-col space-y-6">
-      {/* Name */}
-      <div className="grid w-full items-center gap-1.5">
-        <Label htmlFor="name">Name</Label>
-        <Input type="name" id="name" placeholder="Name" />
-      </div>
+    <FormProvider {...form}>
+      <form>
+        <div className="flex flex-col space-y-6">
+          {/* Name */}
+          <FormInput type="text" name="name" label="Name" placeholderAsLabel />
 
-      {/* Description */}
-      <div className="grid w-full items-center gap-1.5">
-        <Label htmlFor="description">Description</Label>
-        <Input type="description" id="description" placeholder="Description" />
-      </div>
+          {/* Description */}
+          <FormInput type="text" name="description" label="Description" placeholderAsLabel />
 
-      {/* Amount */}
-      <div className="grid w-full items-center gap-1.5">
-        <Label htmlFor="amount">Amount</Label>
-        <Input type="amount" id="amount" placeholder="Amount" />
-      </div>
+          {/* Amount */}
+          <FormInput type="number" name="amount" label="Amount" placeholderAsLabel />
 
-      {/* Users */}
-      <div className="grid w-full items-center gap-2">
-        <Label>Users</Label>
-        <div className="flex flex-wrap gap-4 px-2">
-          <div className="flex gap-2">
-            <Checkbox id="user1" />
-            <Label className="text-xs text-gray-500" htmlFor="user1">
-              User1
-            </Label>
+          {/* Users */}
+          <div className="grid w-full items-center gap-2">
+            <Label>Users</Label>
+            <div className="flex flex-wrap gap-4 px-2">
+              <div className="flex gap-2">
+                <Checkbox id="user1" />
+                <Label className="text-xs text-gray-500" htmlFor="user1">
+                  User1
+                </Label>
+              </div>
+              <div className="flex gap-2">
+                <Checkbox id="user2" />
+                <Label className="text-xs text-gray-500" htmlFor="user2">
+                  User2
+                </Label>
+              </div>
+            </div>
           </div>
-          <div className="flex gap-2">
-            <Checkbox id="user2" />
-            <Label className="text-xs text-gray-500" htmlFor="user2">
-              User2
-            </Label>
+
+          {/* Priority */}
+          <div className="grid w-full items-center gap-2">
+            <Label>Priority</Label>
+            <RadioGroup defaultValue="low" className="flex flex-wrap gap-4 px-2">
+              <div className="flex gap-2">
+                <RadioGroupItem value="high" id="high" />
+                <Label htmlFor="high" className="text-xs text-gray-500">
+                  High
+                </Label>
+              </div>
+              <div className="flex gap-2">
+                <RadioGroupItem value="low" id="low" />
+                <Label htmlFor="low" className="text-xs text-gray-500">
+                  Low
+                </Label>
+              </div>
+            </RadioGroup>
           </div>
+
+          {/* Status */}
+          <div className="grid w-full items-center gap-2">
+            <Label>Status</Label>
+            <RadioGroup defaultValue="public" className="flex flex-wrap gap-4 px-2">
+              <div className="flex gap-2">
+                <RadioGroupItem value="public" id="public" />
+                <Label htmlFor="public" className="text-xs text-gray-500">
+                  Public
+                </Label>
+              </div>
+              <div className="flex gap-2">
+                <RadioGroupItem value="private" id="private" />
+                <Label htmlFor="private" className="text-xs text-gray-500">
+                  Private
+                </Label>
+              </div>
+            </RadioGroup>
+          </div>
+
+          <Button className="m-auto">Submit</Button>
         </div>
-      </div>
-
-      {/* Priority */}
-      <div className="grid w-full items-center gap-2">
-        <Label>Priority</Label>
-        <RadioGroup defaultValue="low" className="flex flex-wrap gap-4 px-2">
-          <div className="flex gap-2">
-            <RadioGroupItem value="high" id="high" />
-            <Label htmlFor="high" className="text-xs text-gray-500">
-              High
-            </Label>
-          </div>
-          <div className="flex gap-2">
-            <RadioGroupItem value="low" id="low" />
-            <Label htmlFor="low" className="text-xs text-gray-500">
-              Low
-            </Label>
-          </div>
-        </RadioGroup>
-      </div>
-
-      {/* Status */}
-      <div className="grid w-full items-center gap-2">
-        <Label>Status</Label>
-        <RadioGroup defaultValue="public" className="flex flex-wrap gap-4 px-2">
-          <div className="flex gap-2">
-            <RadioGroupItem value="public" id="public" />
-            <Label htmlFor="public" className="text-xs text-gray-500">
-              Public
-            </Label>
-          </div>
-          <div className="flex gap-2">
-            <RadioGroupItem value="private" id="private" />
-            <Label htmlFor="private" className="text-xs text-gray-500">
-              Private
-            </Label>
-          </div>
-        </RadioGroup>
-      </div>
-
-      <Button className="m-auto">Submit</Button>
-    </div>
+      </form>
+    </FormProvider>
   );
 };
--- src/db/zod/settle-up.ts
+++ src/db/zod/settle-up.ts
@@ -5,3 +5,11 @@ import { settleUp } from "../schema/activity";
 
 export const settleUpInsertSchema = createInsertSchema(settleUp).omit({ id: true });
 export type SettleUpInsertSchema = z.infer<typeof settleUpInsertSchema>;
+
+export const settleUpFormSchema = settleUpInsertSchema.pick({
+  description: true,
+  isPublic: true,
+  priority: true,
+  name: true
+});
+export type SettleUpForm = z.infer<typeof settleUpFormSchema>;
